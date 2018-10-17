70941c4be13ea2f230e49d096810e5e1eb5854ee
Allen Tong <sleepyxivo@gmail.com>
2018-10-16 22:21:00 -0700

    testing again

--- server/utilities/slack.js
+++ server/utilities/slack.js
@@ -3,20 +3,21 @@ module.exports = { createSlackWrapper };
 function createSlackWrapper(attachment) {
   return {
     response_type: "in_channel",
-    text: "Eat here:",
-    attachments: [
-      {
-        // title: "Name",
-        text: attachment.name
-      },
-      {
-        // title: "Address",
-        text: attachment.address
-      },
-      {
-        // title: "Yelp",
-        text: attachment.yelp
-      }
-    ]
+    text: JSON.stringify(attachment)
+    // text: "Eat here:",
+    // attachments: [
+    //   {
+    //     // title: "Name",
+    //     text: attachment.name
+    //   },
+    //   {
+    //     // title: "Address",
+    //     text: attachment.address
+    //   },
+    //   {
+    //     // title: "Yelp",
+    //     text: attachment.yelp
+    //   }
+    // ]
   };
 }
